9b7359ac755c6529ae8e56a5c4c9a190b6670ca0
Carol B <carolb@bu.edu>
2025-01-31 09:11:05 -0500

    style fixes

--- src/components/Prototype.tsx
+++ src/components/Prototype.tsx
@@ -1,15 +1,10 @@
 export default function Prototype() {
-	// style="border: 1px solid rgba(0, 0, 0, 0.1);"
-	// min-zoom
-	const styles = {
-		border: "1px solid white",
-		borderRadius: "10px",
-	};
 	return (
 		<div className="flex flex-col items-center justify-center h-full w-[75%] gap-y-4">
-			<h1 className="text-white text-2xl">
-				Welcome to the Perimeter Health interactive demo!
+			<h1 className="text-white text-5xl font-title uppercase">
+				Perimeter Health
 			</h1>
+			<h2 className="text-white text-2xl">Welcome to the interactive demo!</h2>
 			<div className="w-[70%]">
 				<p className="text-white">
 					To navigate the prototype, first click on the arrows in the top right
@@ -22,7 +17,7 @@ export default function Prototype() {
 				height="450"
 				src="https://embed.figma.com/proto/RewC7Agb2jK195DjMBSjoa/Perimeter-Health-UI?node-id=74-2671&scaling=scale-down&content-scaling=fixed&page-id=74%3A2329&starting-point-node-id=74%3A2671&embed-host=share"
 				allowFullScreen
-				style={styles}
+				className="border border-white rounded-xl"
 			></iframe>
 		</div>
 	);
